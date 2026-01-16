2847401d2f729c727c56df7860c3b41145a32840
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2026-01-16 12:38:23 +0100

    fix: redirct callback in NextAuth

--- apps/web/src/app/api/auth/[...nextauth]/route.js
+++ apps/web/src/app/api/auth/[...nextauth]/route.js
@@ -17,14 +17,38 @@ const handler = NextAuth({
     },
 
     callbacks: {
-        async redirect({ url, baseUrl }) {
-            // If there's a callbackUrl, use it
-            if (url.startsWith("/")) return `${baseUrl}${url}`;
+        // async redirect({ url, baseUrl }) {
+        //     // Allows relative callback URLs
+        //     if (url.startsWith("/home")) return `${baseUrl}/getting-started`;
+            
+        //     // Allows callback URLs on the same origin
+        //     if (url.startsWith(baseUrl)) return url;
+            
+        //     // Default fallback
+        //     return `/home/getting-started`;
+        // },
+
+        async redirect({url, baseUrl}) {
             // If the url is from the same origin, use it
-            else if (new URL(url).origin === baseUrl) return url;
-            // Otherwise, return to getting-started as fallback
-            return `${baseUrl}/getting-started`;
+            if (url.startsWith("/")) {
+                return url
+            } else {
+                return `${baseUrl}/getting-started`
+            }
         },
+
+        // async redirect({ baseUrl }) {
+        //     return `${baseUrl}/getting-started`
+        // },
+
+        // async redirect({ url, baseUrl }) {
+        //     // If there's a callbackUrl, use it
+        //     if (url.startsWith("/")) return `${baseUrl}${url}`;
+        //     // If the url is from the same origin, use it
+        //     else if (new URL(url).origin === baseUrl) return url;
+        //     // Otherwise, return to getting-started as fallback
+        //     return `${baseUrl}/getting-started`;
+        // },
 
 
         async session({ session, token }) {
